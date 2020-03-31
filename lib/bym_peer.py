6503afe33e5266a7903d061123b6e8f8dd926a98
fpemud <fpemud@sina.com>
2020-03-31 22:34:13 +0800

    fix

---
+++ lib/bym_peer.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# -*- coding: utf-8; tab-width: 4; indent-tabs-mode: t -*-
+
+
+class BymPeer:
+
+    def __init__(self):
+        pass
+
+    def get_peer_id(self):
+        pass
+
+    def start(self):
+        pass
+
+
